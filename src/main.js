248a8524e8e775e605e5a621474ae7f5e79ed22c
notagen <33895863+notagen@users.noreply.github.com>
2025-08-24 00:21:50 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
-gdfgd
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(<App />);
